48bc27b7413a24b530f4f0d355c9081ff3aeabe9
Diogenes <egeyenigezer1919@gmail.com>
2024-04-18 17:54:41 +0300

    #ProcessType ,Product ,Request ,Role sınıfları için Manager sınıfları eklendi.

---
+++ ERPSystem.Business/Concrete/ProductManager.cs
@@ -0,0 +1,105 @@
+﻿using AutoMapper;
+using ERPSystem.Business.Abstract;
+using ERPSystem.DataAccess.Abstract.DataManagement;
+using ERPSystem.Entity.DTO.ProductDTO;
+using ERPSystem.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+
+        public ProductManager(IMapper mapper, IUnitOfWork uow)
+        {
+            _mapper = mapper;
+            _uow = uow;
+        }
+
+        public async Task<ProductDTOResponse> AddAsync(ProductDTORequest RequestEntity)
+        {
+            var product = _mapper.Map<Product>(RequestEntity);
+            var addedProduct = await _uow.ProductRepository.AddAsync(product);
+            await _uow.SaveChangeAsync();
+
+            ProductDTOResponse productDTOResponse = _mapper.Map<ProductDTOResponse>(addedProduct.Entity);
+            return productDTOResponse;
+        }
+
+        public async Task DeleteAsync(ProductDTORequest RequestEntity)
+        {
+            var product = _mapper.Map<Product>(RequestEntity);
+            await _uow.ProductRepository.RemoveAsync(product);
+            await _uow.SaveChangeAsync();
+        }
+
+        public async Task<IEnumerable<ProductDTOResponse>> GetAllAsync(ProductDTORequest RequestEntity)
+        {
+            if (RequestEntity.CategoryName!=null)
+            {
+                var product = _mapper.Map<Product>(RequestEntity);
+                var dbProducts = await _uow.ProductRepository.GetAllAsync(x=>x.CategoryName==product.CategoryName);
+
+                List<ProductDTOResponse> productDTOResponses = new();
+
+                foreach (var dbProduct in dbProducts)
+                {
+                    productDTOResponses.Add(_mapper.Map<ProductDTOResponse>(dbProduct));
+                }
+
+                return productDTOResponses;
+            }
+            else if (RequestEntity.BrandName!=null)
+            {
+                var product = _mapper.Map<Product>(RequestEntity);
+                var dbProducts = await _uow.ProductRepository.GetAllAsync(x => x.BrandName == product.BrandName);
+
+                List<ProductDTOResponse> productDTOResponses = new();
+
+                foreach (var dbProduct in dbProducts)
+                {
+                    productDTOResponses.Add(_mapper.Map<ProductDTOResponse>(dbProduct));
+                }
+
+                return productDTOResponses;
+            }
+            else
+            {
+                var dbProducts = await _uow.ProductRepository.GetAllAsync();
+
+                List<ProductDTOResponse> productDTOResponses = new();
+
+                foreach (var dbProduct in dbProducts)
+                {
+                    productDTOResponses.Add(_mapper.Map<ProductDTOResponse>(dbProduct));
+                }
+
+                return productDTOResponses;
+
+            }
+        }
+
+        public async Task<ProductDTOResponse> GetAsync(ProductDTORequest RequestEntity)
+        {
+            var product = _mapper.Map<Product>(RequestEntity);
+            var dbProduct = await _uow.ProductRepository.GetAsync(x=>x.Id==product.Id);
+
+            ProductDTOResponse productDTOResponse = _mapper.Map<ProductDTOResponse>(dbProduct);
+
+            return productDTOResponse;
+        }
+
+        public async Task UpdateAsync(ProductDTORequest RequestEntity)
+        {
+            var product = _mapper.Map<Product>(RequestEntity);
+            await _uow.ProductRepository.UpdateAsync(product);
+            await _uow.SaveChangeAsync();
+        }
+    }
+}
---
+++ ERPSystem.Business/Concrete/RequestManager.cs
@@ -0,0 +1,103 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration.Annotations;
+using ERPSystem.Business.Abstract;
+using ERPSystem.DataAccess.Abstract.DataManagement;
+using ERPSystem.Entity.DTO.RequestDTO;
+using ERPSystem.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Business.Concrete
+{
+    public class RequestManager : IRequestService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+
+        public RequestManager(IMapper mapper, IUnitOfWork uow)
+        {
+            _mapper = mapper;
+            _uow = uow;
+        }
+
+        public async Task<RequestDTOResponse> AddAsync(RequestDTORequest RequestEntity)
+        {
+            var request = _mapper.Map<Request>(RequestEntity);
+            var addedRequest = await _uow.RequestRepository.AddAsync(request);
+            await _uow.SaveChangeAsync();
+            RequestDTOResponse requestDTOResponse =_mapper.Map<RequestDTOResponse>(addedRequest.Entity);
+            return requestDTOResponse;
+        }
+
+        public async Task DeleteAsync(RequestDTORequest RequestEntity)
+        {
+            var request = _mapper.Map<Request>(RequestEntity);
+            await _uow.RequestRepository.RemoveAsync(request);
+            await _uow.SaveChangeAsync();
+        }
+
+        public async Task<IEnumerable<RequestDTOResponse>> GetAllAsync(RequestDTORequest RequestEntity)
+        {
+            if (RequestEntity.StatusId!=null)
+            {
+                var request = _mapper.Map<Request>(RequestEntity);
+
+                var dbRequests = await _uow.RequestRepository.GetAllAsync(x=>x.StatusId==request.StatusId);
+
+                List<RequestDTOResponse> requestDTOResponses = new();
+
+                foreach (var dbRequest in dbRequests)
+                {
+                    requestDTOResponses.Add(_mapper.Map<RequestDTOResponse>(dbRequest));
+                }
+                return requestDTOResponses;
+            }
+            else if (RequestEntity.ProductId != null)
+            {
+                var request = _mapper.Map<Request>(RequestEntity);
+
+                var dbRequests = await _uow.RequestRepository.GetAllAsync(x => x.ProductId == request.ProductId);
+
+                List<RequestDTOResponse> requestDTOResponses = new();
+
+                foreach (var dbRequest in dbRequests)
+                {
+                    requestDTOResponses.Add(_mapper.Map<RequestDTOResponse>(dbRequest));
+                }
+                return requestDTOResponses;
+            }
+            else
+            {
+                var dbRequests = await _uow.RequestRepository.GetAllAsync();
+
+                List<RequestDTOResponse> requestDTOResponses = new();
+
+                foreach (var dbRequest in dbRequests)
+                {
+                    requestDTOResponses.Add(_mapper.Map<RequestDTOResponse>(dbRequest));
+                }
+                return requestDTOResponses;
+            }
+        }
+
+        public async Task<RequestDTOResponse> GetAsync(RequestDTORequest RequestEntity)
+        {
+            var request = _mapper.Map<Request>(RequestEntity);
+
+            var dbRequest = await _uow.RequestRepository.GetAsync(x=>x.Id==request.Id);
+            RequestDTOResponse requestDTOResponse = _mapper.Map<RequestDTOResponse>(dbRequest);
+            return requestDTOResponse;
+
+        }
+
+        public async Task UpdateAsync(RequestDTORequest RequestEntity)
+        {
+            var request = _mapper.Map<Request>(RequestEntity);
+            await _uow.RequestRepository.UpdateAsync(request);
+            await _uow.SaveChangeAsync();
+        }
+    }
+}
