9a934f21a044d7e13b53db38956125f55b10fb98
Diogenes <egeyenigezer1919@gmail.com>
2024-04-19 17:30:35 +0300

    #Company Controller eklendi. #Company ve Department servisleri program.cs' e tanıtıldı.

---
+++ ERPSystem.API/Controllers/CompanyController.cs
@@ -0,0 +1,36 @@
+﻿using ERPSystem.Business.Abstract;
+using ERPSystem.DataAccess.Abstract;
+using ERPSystem.Entity.DTO.CompanyDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ICompanyService _companyService;
+
+        public CompanyController(ICompanyService companyService)
+        {
+            _companyService = companyService;
+        }
+
+        [HttpPost("/Companies")]
+        public async Task<IActionResult> GetAllAsync(CompanyDTORequest companyDTORequest)
+        {
+            var companies = await _companyService.GetAllAsync(companyDTORequest);
+            return Ok(companies);
+        }
+
+        [HttpPost("/AddCompany")]
+        public async Task<IActionResult> AddAsync(CompanyDTORequest companyDTORequest)
+        {
+            var company = await _companyService.AddAsync(companyDTORequest);
+            return Ok(company);
+        }
+
+
+    }
+}
--- ERPSystem.API/Program.cs
+++ ERPSystem.API/Program.cs
@@ -1,3 +1,9 @@
+using ERPSystem.Business.Abstract;
+using ERPSystem.Business.Concrete;
+using ERPSystem.DataAccess.Abstract.DataManagement;
+using ERPSystem.DataAccess.Concrete.Context;
+using ERPSystem.DataAccess.Concrete.DataManagement;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ERPContext>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IUnitOfWork, EfUnitOfWork>();
+builder.Services.AddScoped<ICompanyService,CompanyManager>();
+builder.Services.AddScoped<IDepartmentService,DepartmentManager>();
+
 
 var app = builder.Build();
 
