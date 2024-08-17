efb384d0b12a98c30d6776dc08d20ddc332b0627
Hakan Demirci <abc-dh2@hotmail.com>
2024-08-17 17:50:01 +0300

    #Map nesneleri ekleniyor.

---
+++ ERPSystem.DataAccess/Map/Base/BaseMap.cs
@@ -0,0 +1,21 @@
+﻿using ERPSystem.Entity.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.DataAccess.Map.Base
+{
+    public class BaseMap<T> : IEntityTypeConfiguration<T> where T :BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id).ValueGeneratedOnAdd();
+            
+        }
+    }
+}
---
+++ ERPSystem.DataAccess/Map/CategoryMap.cs
@@ -0,0 +1,22 @@
+﻿using ERPSystem.DataAccess.Map.Base;
+using ERPSystem.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.DataAccess.Map
+{
+    public class CategoryMap:BaseMap<Category>
+    {
+
+        public override void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+            builder.Property(x=>x.Name).HasMaxLength(500).IsRequired();
+        }
+    }
+}
